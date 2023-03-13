588f032e7169c58addddb62e6834ced66efd88f2
sdsarun <sdsarun@outlook.com>
2023-03-13 13:50:13 +0700

    add test instruction

--- solve.js
+++ solve.js
@@ -1,12 +1,20 @@
-// read input
-let dayNumber = Math.floor(Math.random() * 20);
 
-// store day name
-const daysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function solve(n) {
+    // read input
+    let dayNumber = n;
 
-// check valid dayNumber
-if (dayNumber >= 0 && dayNumber <= 6) {
-    console.log(daysName[dayNumber]);
-} else {
-    console.log("Invalid day number.");
+    // store day name
+    const daysName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    // check valid dayNumber
+    if (dayNumber >= 0 && dayNumber <= 6) {
+        console.log(n, daysName[dayNumber]);
+    } else {
+        console.log(n, "Invalid day number.");
+    }
+}
+
+// testing positive and negative input.
+for (let i = -10; i <= 10; i++) {
+    solve(i);
 }
